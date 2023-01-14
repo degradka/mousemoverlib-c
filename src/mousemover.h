186c8cac79ca78530d90b8bc328aae0c49a8205d
degradka <69397649+degradka@users.noreply.github.com>
2023-01-14 13:52:41 +0300

    Update mousemover.h

--- src/mousemover.h
+++ src/mousemover.h
@@ -23,11 +23,61 @@ void moveMouseTo(int monitorNum, int x, int y);
 /**
  * @brief Trigger a mouse button click.
  * 
- * @param monitorNum The number of your monitor
  * @param buttonNum The mouse button
- * @param clickDelay Delay between the current click and the next click
+ * @param clickDelay Delay in ms before the click
+ * @param buttonHoldTime Time in ms the button will be pressed after the click
  */
-void clickMouse(int monitorNum, int buttonNum, float clickDelay);
+void clickMouseButton(int buttonNum, float clickDelay, float buttonHoldTime);
+
+/**
+ * @brief Trigger a mouse button click.
+ *
+ * @param monitorNum The number of your monitor
+ * @param x X position
+ * @param y Y position
+ * @param buttonNum The mouse button
+ * @param clickDelay Delay in ms before the click
+ * @param buttonHoldTime Time in ms the button will be pressed after the click
+ */
+void clickMouseButtonAt(int monitorNum, int x, int y, int buttonNum, float clickDelay, float buttonHoldTime);
+
+/**
+ * @brief Trigger a mouse button double-click.
+ *
+ * @param buttonNum The mouse button
+ * @param firstClickDelay Delay in ms before the first click
+ * @param secondClickDelay Delay in ms before the second click
+ * @param buttonHoldTime Time in ms each button will be pressed after the click
+ */
+void doubleClickMouseButton(int buttonNum, float firstClickDelay, float secondClickDelay, float buttonHoldTime);
+
+/**
+ * @brief Get screen width of a specified monitor.
+ *
+ * @param monitorNum The number of your monitor
+ */
+int getScreenWidth(int monitorNum);
+
+/**
+ * @brief Get screen height of a specified monitor.
+ *
+ * @param monitorNum The number of your monitor
+ */
+int getScreenHeight(int monitorNum);
+
+/**
+ * @brief Get cursor position X in a specified monitor.
+ *
+ * @param monitorNum The number of your monitor
+ */
+int getMouseX(int monitorNum);
+
+/**
+ * @brief Get cursor position Y in a specified monitor.
+ *
+ * @param monitorNum The number of your monitor
+ */
+int getMouseY(int monitorNum);
 
 #ifdef __cplusplus
 }
