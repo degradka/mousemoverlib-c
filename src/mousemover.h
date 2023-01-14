1a17b1aa74550cec2e437764dd4200f0cb6f2b29
mamalord <supermamalord@gmail.com>
2023-01-14 17:23:34 +0300

    Misc documentation changes

--- src/mousemover.h
+++ src/mousemover.h
@@ -5,12 +5,29 @@
 extern "C" {
 #endif
 
+enum {
+    LMB = 1, // Left Mouse Button
+    MMB,     // Middle Mouse Button
+    RMB,     // Right Mouse Button
+    MWU,     // Mouse Wheel Up 
+    MWD,     // Mouse Wheel Down
+};
+/*
+// Left Mouse Button
 #define LMB 1
-#define MMB 2
-#define RMB 3
-#define MWU 4
-#define MWD 5
 
+// Middle Mouse Button
+#define MMB 2
+
+// Right Mouse Button
+#define RMB 3
+
+// Mouse Wheel Up
+#define MWU 4
+
+// Mouse Wheel Down
+#define MWD 5
+*/
 /**
  * @brief Move the mouse cursor into the specified position on a specified monitor.
  * 
@@ -25,7 +42,7 @@ void moveMouseTo(int monitorNum, int x, int y);
  * 
  * @param buttonNum The mouse button
  * @param clickDelay Delay in ms before the click
- * @param buttonHoldTime Time in ms the button will be pressed after the click
+ * @param buttonHoldTime Time in ms the button will be held after the click
  */
 void clickMouseButton(int buttonNum, float clickDelay, float buttonHoldTime);
 
@@ -37,7 +54,7 @@ void clickMouseButton(int buttonNum, float clickDelay, float buttonHoldTime);
  * @param y Y position
  * @param buttonNum The mouse button
  * @param clickDelay Delay in ms before the click
- * @param buttonHoldTime Time in ms the button will be pressed after the click
+ * @param buttonHoldTime Time in ms the button will be held after the click
  */
 void clickMouseButtonAt(int monitorNum, int x, int y, int buttonNum, float clickDelay, float buttonHoldTime);
 
@@ -47,7 +64,7 @@ void clickMouseButtonAt(int monitorNum, int x, int y, int buttonNum, float click
  * @param buttonNum The mouse button
  * @param firstClickDelay Delay in ms before the first click
  * @param secondClickDelay Delay in ms before the second click
- * @param buttonHoldTime Time in ms each button will be pressed after the click
+ * @param buttonHoldTime Time in ms each button will be held after the click
  */
 void doubleClickMouseButton(int buttonNum, float firstClickDelay, float secondClickDelay, float buttonHoldTime);
 
